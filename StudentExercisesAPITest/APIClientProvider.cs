93eadf18f9c849dc5ce54e6e87a8f5fabb25d166
Swathi Mukkamala <swathikmukkamala@gmail.com>
2020-08-14 16:06:15 -0400

    worked on integration testing

---
+++ StudentExercisesAPITest/APIClientProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace StudentExercisesAPITest
+{
+    class APIClientProvider : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        public HttpClient Client { get; private set; }
+        private readonly WebApplicationFactory<Startup> _factory = new WebApplicationFactory<Startup>();
+        public APIClientProvider()
+        {
+            Client = _factory.CreateClient();
+        }
+        public void Dispose()
+        {
+            _factory?.Dispose();
+            Client?.Dispose();
+        }
+    }
+}
---
+++ StudentExercisesAPITest/DatabaseFixture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+using StudentExercisesAPI.Models;
+
+namespace StudentExercisesAPITest
+{
+    public class DatabaseFixture : IDisposable
+    {
+        private readonly string ConnectionString = @$"Server=localhost\SQLEXPRESS;Database=StudentExercises;Trusted_Connection=True;";
+        public Student TestStudent { get; set; }
+        public DatabaseFixture()
+        {
+            Student newStudent = new Student
+            {
+                
+            };
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @$"INSERT INTO Student (FirstName, LastName, SlackHandle, CohortId)
+                                        OUTPUT INSERTED.Id
+                                        VALUES ('{newStudent.FirstName}', '{newStudent.LastName}', '{newStudent.SlackHandle}', '{newStudent.CohortId}')";
+                    int newId = (int)cmd.ExecuteScalar();
+                    newStudent.Id = newId;
+                    TestStudent = newStudent;
+                }
+            }
+        }
+        public void Dispose()
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @$"DELETE FROM Coffee WHERE Title='Test Coffee'";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
